369ea019696aff7bca7336fef32fb8cc19f97ee4
Ratmir <ratmir.yuldashev28@mail.ru>
2023-11-16 00:51:19 +0300

    starting program

---
+++ Formator/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        TestGUI app = new TestGUI();
+        app.setVisible(true);
+    }
+}
---
+++ Java/lesson java/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.SortedMap;
+import java.util.SortedSet;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
