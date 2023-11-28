e2f8d3023b9344247e58f673c2478585108cf234
Ratmir <ratmir.yuldashev28@mail.ru>
2023-11-28 15:54:13 +0300

    -

--- database/PostgresSQL/Комиссия(4).sql
+++ database/PostgresSQL/Комиссия(4).sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE UpdateStudentAddress(IN studentID INT, IN newAddress VARCHAR(255))
+CREATE OR REPLACE PROCEDURE UpdateStudentAddress(studentID INT, newAddress VARCHAR(255))
 BEGIN
   UPDATE Students SET Address = newAddress WHERE ID_Student = studentID;
 END;
