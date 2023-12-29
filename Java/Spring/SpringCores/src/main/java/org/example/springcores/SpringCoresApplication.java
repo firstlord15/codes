a3c580b87d8feec081e25846c901f7c16ba20992
Ogi <84020524+AmirovOgi@users.noreply.github.com>
2023-12-29 16:36:42 +0300

    first commit

---
+++ Java/Spring/SpringCores/src/main/java/org/example/springcores/SpringCoresApplication.java
@@ -0,0 +1,13 @@
+package org.example.springcores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCoresApplication.class, args);
+    }
+
+}
