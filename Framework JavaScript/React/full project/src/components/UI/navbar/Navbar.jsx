b69a56489e855b668a7c6c0693708a51ada18c05
ratmir <ratmir.yuldashev28@mail.ru>
2024-01-25 10:46:26 +0300

    update

--- Framework JavaScript/React/full project/src/components/UI/navbar/Navbar.jsx
+++ Framework JavaScript/React/full project/src/components/UI/navbar/Navbar.jsx
@@ -5,9 +5,9 @@ import '../../../styles/App.css'
 const Navbar = () => {
 	return (
 		<div className='navbar'>
-			<div className='navbar__links'>
-				<Link href='/about'>О сайте</Link>
-				<Link href='/posts'>Посты</Link>
+			<div className='navbar__links nav'>
+				<Link to='/about'>О сайте</Link>
+				<Link to='/posts'>Посты</Link>
 			</div>
 		</div>
 	)
--- Testing/2 семестр/1/main.py
+++ Testing/2 семестр/1/main.py
@@ -1,11 +1,44 @@
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+import time
+from random import randint
+from selenium.webdriver.common.by import By
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
 
-options = Options()
-service = Service()
-browser = webdriver.Chrome(service=service, options = options)
-options = Options()
-browser.get("https://www.google.com")
-assert browser.title == "Google"
-browser.quit()
+driver = webdriver.Chrome(service=ChromeService())
+driver.get("https://konflic.github.io/examples/")
+
+input_field = driver.find_element(By.CSS_SELECTOR, "#inp")
+inp_form = driver.find_element(By.CSS_SELECTOR, "#inp_form")
+text = "Давид Дебил!"
+ 
+input_field.click()
+input_field.clear()
+for letter in text:
+    time.sleep(randint(1, 3)/10)
+    input_field.send_keys(letter)
+time.sleep(1)
+
+inp_form.click()
+inp_form.clear()
+for letter in text:
+    time.sleep(randint(1, 3)/10)
+    inp_form.send_keys(letter)
+
+
+div = driver.find_element(By.CSS_SELECTOR, "div.margins:nth-child(6) table.table.table-bordered tbody:nth-child(1) tr:nth-child(3) > td:nth-child(1)")
+
+div.click()
+time.sleep(1)
+div.click()
+time.sleep(1)
+div.click()
+time.sleep(1)
+div.click()
+
+
+
+
+
+time.sleep(3)
+
+driver.quit()
